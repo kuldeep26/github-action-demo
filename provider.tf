429f1d8a1b6d6ca1bc174d7f0947910ac0233e00
kuldeep26 <kuldeepsingh260893@gmail.com>
2023-06-25 20:26:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 terraform {
   backend "s3" {
     bucket = var.tfstate_bucket_name
     key    = "usregion/terraform.tfstate"
     region = var.region
   }
-=======
-terraform {
-  backend "s3" {
-    bucket = var.tfstate_bucket_name
-    key    = "usregion/terraform.tfstate"
-    region = var.region
-  }
->>>>>>> 11a36cd5e6c63f0ec0826280ad4e4a6917d1b80a
-}
