ce4f3371c226ee3b6d184192ec4328e13fed32a5
kuldeep26 <kuldeepsingh260893@gmail.com>
2023-06-25 18:48:38 +0530

    adding bucket ownership code

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "demo_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "demo_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.demo_bucket_ownership]
   bucket = aws_s3_bucket.demo_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "demo_bucket_ownership" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
